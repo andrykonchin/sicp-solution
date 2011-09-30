d1202c1db4ab826bb8bab4a7fc19de887a3552cf
aaz <andry.konchin@gmail.com>
2011-10-01 00:10:52 +0300

    Fix task 2.8

--- chapters/chapter2/task02_08.rkt
+++ chapters/chapter2/task02_08.rkt
@@ -9,7 +9,7 @@
 
 (define (sub-interval x y)
   (make-interval (- (lower-bound x) (upper-bound y))
-                 (+ (upper-bound x) (upper-bound y))))
+                 (- (upper-bound x) (lower-bound y))))
 
 (define (make-interval a b) (cons a b))
 
